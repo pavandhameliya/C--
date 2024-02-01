869b9a5d76fe025828c0ce6323264d77689b50c9
pavan dhameliya <pavandhameliya@pavans-MacBook-Pro.local>
2024-02-01 11:45:05 +0530

    pavan

---
+++ c.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+
+  int a, b, c;
+
+  cout << " anter a :";
+  cin >> a;
+
+  cout << " anter b :";
+  cin >> b;
+
+  c = a + b;
+
+  cout << "sum is " << c;
+  // cout<< end;
+
+  return 0;
+}
